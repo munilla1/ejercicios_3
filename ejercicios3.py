fa1f28cad696cd7053d07b217279e7bfdaa3ce8b
munilla1 <mucho-muni@hotmail.com>
2022-03-31 16:41:00 +0200

    voy por el ejercicio 24

---
+++ ejercicios3.py
@@ -0,0 +1,22 @@
+print ("------------ejercicio numero 23:-------------")
+la_suma = 0
+for sumatorio in range(0,100+1):
+    print(sumatorio)
+    la_suma = la_suma + sumatorio
+print(la_suma)
+
+
+print ("------------ejercicio numero 24:-------------")
+numero = int(14)
+contador = 0
+for divisor in range(1, numero + 1):
+    if (numero % divisor) == 0:
+        print(divisor)
+        contador += 1
+#print(numero, contador)
+
+
+print ("------------ejercicio numero 24:-------------")
+texto = ["verde que te quiero verde"]
+for x in "hola":
+    print(x)
